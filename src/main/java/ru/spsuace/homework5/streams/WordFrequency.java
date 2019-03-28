7f742ae8a4a34c7b331ce5a4ed614d82b5aa911f
ELena <evseevalenoshka>
2019-03-29 00:01:10 +0300

    Word frequency

--- src/main/java/ru/spsuace/homework5/streams/WordFrequency.java
+++ src/main/java/ru/spsuace/homework5/streams/WordFrequency.java
@@ -1,7 +1,11 @@
 package ru.spsuace.homework5.streams;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Comparator;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.function.*;
 
 /**
  * Написать программу, которая из текста (стрим строк), возвращает 10 самых популярных слов (В порядке убывания частоты).
@@ -21,8 +25,17 @@ public class WordFrequency {
      * Без них - 4 балла
      */
     public static List<String> wordFrequency(Stream<String> lines) {
-        return null;
+
+        return lines.map(s -> Stream.of(s.split(" "))
+                .collect(Collectors.toList()))
+                .flatMap(str -> str.stream())
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .map(key -> key.getKey())
+                .collect(Collectors.toList());
     }
-
-
 }
